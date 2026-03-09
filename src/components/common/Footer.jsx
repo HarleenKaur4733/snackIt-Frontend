30bc1aceb477b1213e90a696d1e4af6dc06ae376
Harleen <harlin7814@gmail.com>
2026-03-10 00:33:19 +0530

    Services created

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Food App. All Rights Reserved.</p>
+        <div className="footer-links">
+          <a href="/home" className="footer-link">
+            Terms of Service
+          </a>
+          <a href="/home" className="footer-link">
+            Privacy Psolicy
+          </a>
+          <a href="/home" className="footer-link">
+            Contact US
+          </a>
+        </div>
+      </div>
+      s
+    </footer>
+  );
+};
+export default Footer;
